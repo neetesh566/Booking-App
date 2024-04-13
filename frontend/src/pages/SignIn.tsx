8127b0c952261ce2731b84a84cd7ffabf83aace8
neetesh566 <shakyaneetesh2@gmail.com>
2024-04-13 23:46:18 +0530

    optimise signin and signout

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,34 +1,37 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
-import * as apiClient from '../api-client';
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export type SignInFormData = {
   email: string;
   password: string;
 };
 const SignIn = () => {
-    const {showToast} = useAppContext();
-    const navigate =useNavigate();
+  const { showToast } = useAppContext();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const {
     register,
     formState: { errors },
-    handleSubmit
+    handleSubmit,
   } = useForm<SignInFormData>();
 
   const mutation = useMutation(apiClient.signIn, {
     onSuccess: async () => {
-       showToast({message:"Sign in Successful!", type:"SUCCESS"});
-        navigate("/")   
-    }, onError:(error:Error)=>{
-        showToast({message:error.message,type:"ERROR"});
-    }
+      showToast({ message: "Sign in Successful!", type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken");
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
   });
 
-  const onSubmit = handleSubmit((data)=>{
-    mutation.mutate(data)
-  })
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
   return (
     <form className="flex flex-col gap-5 px-52" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold"> Sign In</h2>
@@ -61,15 +64,20 @@ const SignIn = () => {
         )}
       </label>
       <span>
-        <button
-          type="submit"
-          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
-        >
-          Login
-        </button>
+        <span className="text-sm">
+          Not Registered ?{" "}
+          <Link className="underline" to=" /register ">
+            Create an account here
+          </Link>
+        </span>
       </span>
+      <button
+        type="submit"
+        className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+      >
+        Login
+      </button>
     </form>
-
   );
 };
 
