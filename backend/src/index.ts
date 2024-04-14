de1b6acb6fe817561085208438110681b22bdb53
neetesh566 <shakyaneetesh2@gmail.com>
2024-04-14 18:42:45 +0530

    auth deployment setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/user";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -23,6 +24,7 @@ app.get("/api/test", async (req: Request, res: Response) => {
   res.json({ message: "hello from express endpoint" });
 });
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
