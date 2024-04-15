0d2c66727f630bae4df911031d8b4b13e0b8991d
neetesh566 <shakyaneetesh2@gmail.com>
2024-04-16 00:40:50 +0530

    hotel api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,13 +6,14 @@ import userRoutes from "./routes/user";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from "./routes/my-hotels";
 
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -34,7 +35,7 @@ app.get("/api/test", async (req: Request, res: Response) => {
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-
+app.use("/api/my-hotels", myHotelRoutes);
 app.listen(7000, () => {
   console.log("server running on localhost :7000");
 });
