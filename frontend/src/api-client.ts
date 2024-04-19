b986fafe766b83bf3acbef24a1be854b682e1464
neetesh566 <shakyaneetesh2@gmail.com>
2024-04-19 19:39:38 +0530

    My Hotel View

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,7 @@
 // import ManageHotelForm from "./forms/ManageHotelForm/ManageHotelForm";
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
+import {HotelType} from '../../backend/src/models/hotel'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -68,10 +69,19 @@ export const addMyHotel = async (hotelFormData: FormData) => {
     body: hotelFormData,
   });
 
-  if(!response.ok){
+  if (!response.ok) {
     throw new Error("Failed to add hotel");
   }
 
   return response.json();
+};
 
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    credentials: "include",
+  });
+  if(!response.ok){
+    throw new Error("Error fetching hotels");
+  }
+  return response.json();
 };
